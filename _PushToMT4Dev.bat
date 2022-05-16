a6dc4b3962d82289c85499eb3bcf29c33ff79641
tqbrowne <81526338+tqbrowne@users.noreply.github.com>
2022-05-16 00:16:41 -0400

    test

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -1,13 +1,13 @@
-rem Script to Deploy files from Version Control repository to All Terminals
-rem Use when you need to publish all files to All Terminals
+rem Script to Deploy files from Version Control repository to Development Terminal
+rem Use in case some content needs to be replaced (reverted from Version Control History)
 
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_D"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\FALCON_D"
+:: Source Directory where Version Control Repository is located
+set SOURCE_DIR="%PATH_DSS_Repo%\FALCON_B"
+:: Destination Directory where Expert Advisor is located
+set DEST_DIR="%PATH_T2_E%\04_FALCON_B"
 
-ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
-
-
+ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
 
